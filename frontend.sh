f99fa42f591753dc6b8b4411af75eb6bdd53b41d
jagadeesh3737 <jagadeeshvarma444@gmail.com>
2023-11-08 17:08:20 +0530

    backend

--- frontend.sh
+++ frontend.sh
@@ -1,34 +1,36 @@
+log_file=/tmp/expense.log
+
 echo -e "\e[31m installing nginx \e[0m"
 dnf install nginx -y &>>/tmp/expense.log
-
+echo $?
 
 echo -e "\e[31m enabling nginx \e[0m"
-systemctl enable nginx &>>/tmp/expense.log
-
+systemctl enable nginx &>>log_file
+echo $?
 
 echo -e "\e[31m starting nginx \e[0m"
-systemctl start nginx &>>/tmp/expense.log
+systemctl start nginx &>>log_file
 echo $?
 
 echo -e "\e[31m removing default content \e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
+rm -rf /usr/share/nginx/html/* &>>log_file
 echo $?
 
 echo -e "\e[31m coopying the expense.conf \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
 echo $?
 
 echo -e "\e[31m downloading the frontend application code \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
 echo $?
 
 cd /usr/share/nginx/html
 echo $?
 
 echo -e "\e[31m unzipping the frontend content \e[0m"
-unzip /tmp/frontend.zip &>>/tmp/expense.log
+unzip /tmp/frontend.zip &>>log_file
 echo $?
 
 echo -e "\e[31m restarting nginx \e[0m"
-systemctl restart nginx &>>/tmp/expense.log
+systemctl restart nginx &>>log_file
 echo $?
