276c98ed294056ac83e9ad3aae2cf5adc956d06d
jagadeesh3737 <jagadeeshvarma444@gmail.com>
2023-11-08 17:05:20 +0530

    backend

--- frontend.sh
+++ frontend.sh
@@ -8,25 +8,27 @@ systemctl enable nginx &>>/tmp/expense.log
 
 echo -e "\e[31m starting nginx \e[0m"
 systemctl start nginx &>>/tmp/expense.log
-
+echo $?
 
 echo -e "\e[31m removing default content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
-
+echo $?
 
 echo -e "\e[31m coopying the expense.conf \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
-
+echo $?
 
 echo -e "\e[31m downloading the frontend application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
-
+echo $?
 
 cd /usr/share/nginx/html
+echo $?
 
 echo -e "\e[31m unzipping the frontend content \e[0m"
 unzip /tmp/frontend.zip &>>/tmp/expense.log
-
+echo $?
 
 echo -e "\e[31m restarting nginx \e[0m"
 systemctl restart nginx &>>/tmp/expense.log
+echo $?
