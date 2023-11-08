614476cfb7bad7fa3f59774b66e8de459dc00567
jagadeesh3737 <jagadeeshvarma444@gmail.com>
2023-11-08 17:16:43 +0530

    backend

--- mysql.sh
+++ mysql.sh
@@ -1,17 +1,27 @@
-echo -e "\e[31m disabling mysql \e[0m"
-dnf module disable mysql -y
+log_file=/tmp/expense.log
+color="\e[31m"
 
-echo -e "\e[31 copying mysql.repo \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo
 
-echo -e "\e installing mysql community server \e[0m"
-dnf install mysql-community-server -y
+echo -e "${color} disabling mysql \e[0m"
+dnf module disable mysql -y &>>log_file
+echo $?
 
-echo -e "\e[31m enabling mysql \e[0m"
-systemctl enable mysqld
+echo -e "${color} copying mysql.repo \e[0m"
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
+echo $?
 
-echo -e "\e[31m starting mysql server \e[0m"
-systemctl start mysqld
+echo -e "${color} installing mysql community server \e[0m"
+dnf install mysql-community-server -y &>>log_file
+echo $?
 
-echo -e "\e[31m setting pass for access the service"
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo -e "${color} enabling mysql \e[0m"
+systemctl enable mysqld &>>log_file
+echo $?
+
+echo -e "${color} starting mysql server \e[0m"
+systemctl start mysqld &>>log_file
+echo $?
+
+echo -e "${color} setting pass for access the service"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file
+echo $?
