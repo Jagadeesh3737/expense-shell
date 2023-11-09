8fc13b1c52d953fbf0d59e33f743e1768050c73d
jagadeesh3737 <jagadeeshvarma444@gmail.com>
2023-11-09 14:04:11 +0530

    backend

--- mysql.sh
+++ mysql.sh
@@ -3,25 +3,25 @@ color="\e[35m"
 
 
 echo -e "${color} disabling mysql \e[0m"
-dnf module disable mysql -y &>>log_file
+dnf module disable mysql -y &>>$log_file
 echo $?
 
 echo -e "${color} copying mysql.repo \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 echo $?
 
 echo -e "${color} installing mysql community server \e[0m"
-dnf install mysql-community-server -y &>>log_file
+dnf install mysql-community-server -y &>>$log_file
 echo $?
 
 echo -e "${color} enabling mysql \e[0m"
-systemctl enable mysqld &>>log_file
+systemctl enable mysqld &>>$log_file
 echo $?
 
 echo -e "${color} starting mysql server \e[0m"
-systemctl start mysqld &>>log_file
+systemctl start mysqld &>>$log_file
 echo $?
 
 echo -e "${color} setting pass for access the service"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
 echo $?
